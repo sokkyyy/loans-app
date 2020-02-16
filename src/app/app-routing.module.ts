f41e7e3207b178f8c2929fefb70fed05198aca97
sokkyyy <ndegwaraymond@gmail.com>
2020-02-16 13:55:10 +0300

    Added routing to application form.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ApplicationFormComponent } from './application-form/application-form.component';
+import { LandingComponent } from './landing/landing.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:LandingComponent},
+  { path:'apply', component:ApplicationFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
